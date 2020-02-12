24bcad55685808b6d0a1b6a50b18cf8a0d561106
Brendon Barnes <brenbrenofspace@gmail.com>
2020-02-12 09:10:46 -0500

    added 'zuckpass' command, allows you to try a password against the ZWAPI

--- cogs/general.py
+++ cogs/general.py
@@ -1,5 +1,11 @@
 from discord.ext import commands
 from cogs.util import check
+import pytest
+import time
+import csv
+import requests
+import pandas
+import json
 
 
 class General(commands.Cog):
@@ -20,6 +26,15 @@ class General(commands.Cog):
         """| Allows you to send a question directly to where the bot is run, you may or may not get a response."""
         print(f"Interrogative: {arg}")
         await ctx.send(input("Response: "))
+
+    @commands.command()
+    async def zuckpass(self, ctx, *, arg):
+        """| Checks a single password against the Zuckwatch API"""
+        response = requests.post('https://k1ozwahixa.execute-api.us-east-1.amazonaws.com/dev/password',json={'password': arg})
+        if response.status_code != 400:
+            await ctx.send("Your submitted password is correct.")
+        else:
+            await ctx.send("Your submitted password is incorrect.")
 
 
 def setup(bot):
