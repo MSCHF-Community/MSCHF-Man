b16c8e7a927900805d85716fbafe8b534022f098
Brendon Barnes <brenbrenofspace@gmail.com>
2020-02-16 18:08:41 -0500

    zuckpass can only be used in the server's channel for it

--- cogs/zuckwatch.py
+++ cogs/zuckwatch.py
@@ -7,18 +7,24 @@ import requests
 import pandas
 import json
 
+def is_passchannel(ctx):
+    return ctx.message.channel.id == 678708790878797825
+
+
 class Zuckwatch(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def zuckpass(self, ctx, *, arg):
+    @commands.check(is_passchannel)
+    async def zuckpass(self, ctx, *, password):
         """| Checks a single password against the Zuckwatch API"""
-        response = requests.post('https://k1ozwahixa.execute-api.us-east-1.amazonaws.com/dev/password',json={'password': arg})
-        if response.status_code != 400:
-            await ctx.send("Your submitted password is correct.")
-        else:
-            await ctx.send("Your submitted password is incorrect.")
+        async with ctx.typing():
+            response = requests.post('https://k1ozwahixa.execute-api.us-east-1.amazonaws.com/dev/password',json={'password': password})
+            if response.status_code != 400:
+                await ctx.send("Your submitted password is correct.")
+            else:
+                await ctx.send("Your submitted password is incorrect.")
 
 def setup(bot):
     bot.add_cog(Zuckwatch(bot))
