bc5efc1562e6a34728044f3a749809cdb7000a7c
jarrett-g <jgates29413@gmail.com>
2020-02-24 19:47:02 -0700

    Update afterdark.py
    Add new commands and images

--- cogs/afterdark.py
+++ cogs/afterdark.py
@@ -6,12 +6,6 @@ import discord
 def is_afterdark(ctx):
     return ctx.message.guild.id == 681243283774504981
 
-dercPics = ["sexyderc.jpeg", "derc2.png", "derc3.jpg"]
-
-brenPics = ["bren1.jpg", "bren2.jpg", "bren3.jpg"]
-
-catPics = ["brencat1.jpg", "brencat2.jpg", "brencat3.jpg", "brencat4.jpg", "brencat5.jpg", "jarrettcat1.jpg", "kenzcat1.jpg", "plantcat1.jpg", "plantcat2.png"]
-
 def is_catchannel(ctx): #only exists to control usage
     """Checks if the channel is cat channel in afterdark."""
     return ctx.message.channel.id == 681361617845354506
@@ -20,20 +14,48 @@ def is_eyeschannel(ctx): #only exists to control usage
     """Checks if the channel is eyes channel in afterdark."""
     return ctx.message.channel.id == 681248921250693151
 
+def is_adgeneral(ctx):
+    """Checks if the channel is mod general channel in afterdark."""
+    return ctx.message.channel.id == 681243283774504984
+
+dercPics = ["sexyderc.jpeg", "derc2.png", "derc3.jpg"]
+
+brenPics = ["bren1.jpg", "bren2.jpg", "bren3.jpg"]
+
+kenzPics = ["kenz1.jpg", "elbow1.jpg", "elbow2.jpg", "elbow3.jpg", "elbow4.jpg", "elbow5.jpg"]
+
+jarrettPics = ["jarrett1.jpeg", "jarrett2.png"]
+
+catPics = ["brencat1.jpg", "brencat2.jpg", "brencat3.jpg", "brencat4.jpg", "brencat5.jpg", "jarrettcat1.jpg", "kenzcat1.jpg", "plantcat1.jpg", "plantcat2.png", "plantcat3.jpg", "plantcat4.jpg", "plantcat5.jpg", "plantcat6.jpg", "gothcat.png"]
+
 class afterdark(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(hidden=True)
     @commands.check(is_eyeschannel)
+    @commands.check(is_adgeneral)
     async def hornforderc(self, ctx):
-        """| Allows you to horn over Derc, the prettiest girl in zuckwatch."""
+        """| Allows you to horn for Derc."""
         await ctx.send(file=discord.File(random.choice(dercPics)))
 
     @commands.command(hidden=True)
     @commands.check(is_eyeschannel)
+    @commands.check(is_adgeneral)
     async def hornforbren(self, ctx):
         await ctx.send(file=discord.File(random.choice(brenPics)))
+
+    @commands.command(hidden=True)
+    @commands.check(is_eyeschannel)
+    @commands.check(is_adgeneral)
+    async def unhornforkenz(self, ctx):
+        await ctx.send(file=discord.File(random.choice(kenzPics)))
+
+    @commands.command(hidden=True)
+    @commands.check(is_eyeschannel)
+    @commands.check(is_adgeneral)
+    async def unhornforj(self, ctx):
+        await ctx.send(file=discord.File(random.choice(jarrettPics)))
 
     @commands.command(hidden=True)
     @commands.check(is_afterdark)
