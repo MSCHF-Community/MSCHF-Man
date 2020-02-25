d35fea5d6f5b147003bc120288bd316562081d84
Brendon Barnes <brenbrenofspace@gmail.com>
2020-02-24 22:47:04 -0500

    consistified zuckpass command and formally added "zp" alias
    Signed-off-by: Brendon Barnes <brenbrenofspace@gmail.com>

--- cogs/zuckwatch.py
+++ cogs/zuckwatch.py
@@ -47,8 +47,8 @@ class Zuckwatch(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     @commands.check(is_passchannel)
-    @commands.command()
-    async def zp(self, ctx, *, passwordtry):
+    @commands.command(aliases=['zp'])
+    async def zuckpass(self, ctx, *, passwordtry):
         """| Checks a single password against the Zuckwatch API"""
         async with ctx.typing():
             test_testloginzuck(passwordtry)
