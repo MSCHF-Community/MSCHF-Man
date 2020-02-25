fbb8c280d7536ba46e1dd9025e54568d04b56f20
jarrett-g <jgates29413@gmail.com>
2020-02-25 15:32:53 -0700

    Revert "Update afterdark.py"
    This reverts commit 12e835e96f4533e198b538fe9bb135de7c9d7859.

--- cogs/afterdark.py
+++ cogs/afterdark.py
@@ -6,7 +6,6 @@ import discord
 def is_afterdark(ctx):
     return ctx.message.guild.id == 681243283774504981
 
-#Deprecate?
 def is_catchannel(ctx): #only exists to control usage
     """Checks if the channel is cat channel in afterdark."""
     return ctx.message.channel.id == 681361617845354506
@@ -14,10 +13,6 @@ def is_catchannel(ctx): #only exists to control usage
 def is_eyeschannel(ctx): #only exists to control usage
     """Checks if the channel is eyes channel in afterdark."""
     return ctx.message.channel.id == 681248921250693151
-
-def is_modscategory(ctx): #testing command, can replace is_eyeschannel if testing succeeds
-    """Checks if the category is the mods category"""
-    return ctx.message.category.id = 681381577099313187
 
 #def is_adgeneral(ctx):
 #    """Checks if the channel is mod general channel in afterdark."""
@@ -40,28 +35,28 @@ class afterdark(commands.Cog):
         self.bot = bot
 
     @commands.command(hidden=True)
-    @commands.check(is_modscategory)
+    @commands.check(is_eyeschannel)
     async def hornforderc(self, ctx):
         """| Allows you to horn for Derc."""
         await ctx.send(file=discord.File(random.choice(dercPics)))
 
     @commands.command(hidden=True)
-    @commands.check(is_modscategory)
+    @commands.check(is_eyeschannel)
     async def hornforbren(self, ctx):
         await ctx.send(file=discord.File(random.choice(brenPics)))
 
     @commands.command(hidden=True)
-    @commands.check(is_modscategory)
+    @commands.check(is_eyeschannel)
     async def unhornforkenz(self, ctx):
         await ctx.send(file=discord.File(random.choice(kenzPics)))
 
     @commands.command(hidden=True)
-    @commands.check(is_modscategory)
+    @commands.check(is_eyeschannel)
     async def hornforj(self, ctx):
         await ctx.send(file=discord.File(random.choice(jarrettPics)))
 
     @commands.command(hidden=True)
-    @commands.check(is_modscategory)
+    @commands.check(is_eyeschannel)
     async def hornforplant(self, ctx):
         await ctx.send(file=discord.File(random.choice(plantPics)))
 
