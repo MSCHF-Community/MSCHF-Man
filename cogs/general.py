24c6f276f5b604d7a7a2a991b4561d6160d996b0
Brendon Barnes <brenbrenofspace@gmail.com>
2020-05-14 20:25:07 -0400

    Began refactoring and removed interrogative command

--- cogs/general.py
+++ cogs/general.py
@@ -1,15 +1,4 @@
 from discord.ext import commands
-from cogs.util import check
-import pytest
-import time
-import csv
-import requests
-import pandas
-import json
-import discord
-import io
-import aiohttp
-
 
 class General(commands.Cog):
     def __init__(self, bot):
@@ -18,12 +7,6 @@ class General(commands.Cog):
     @commands.command()
     async def repeat(self, ctx, *, arg): #it repeats what you feed it, got it?
         await ctx.send(f"{arg}")
-
-    @commands.command()
-    async def interrogative(self, ctx, *, arg): #this is a more "fun" commmand, allowing people to talk directly to where the bot is run. Don't try to reload this cog with a pending interrogative, it fucks everything up
-        """| Allows you to send a question directly to where the bot is run, you may or may not get a response."""
-        print(f"Interrogative: {arg}")
-        await ctx.send(input("Response: "))
 
 def setup(bot):
     bot.add_cog(General(bot))
--- cogs/zuckwatch.py
+++ cogs/zuckwatch.py
@@ -1,61 +1,39 @@
 #-----imports-----
-from discord.ext import commands
-from cogs.util import check
-import pytest
-import time
-import csv
-import requests
-import pandas
-import json
-import pytest
-import time
-import jthon
-import sys
-import datetime
-import selenium
 import time as t
-from sys import stdout
-from optparse import OptionParser
-from selenium.common.exceptions import NoSuchElementException
-from webdriver_manager.chrome import ChromeDriverManager
+from optparse import OptionParser #TODO: Update to a non-deprecated module
+import jthon
+from discord.ext import commands
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(chrome_options=options, executable_path=ChromeDriverManager().install())
-config = jthon.load('config')
+from webdriver_manager.chrome import ChromeDriverManager
+
+OPTIONS = webdriver.ChromeOptions()
+OPTIONS.add_argument('--headless')
+OPTIONS.add_argument('--no-sandbox')
+OPTIONS.add_argument('--disable-dev-shm-usage')
+DRIVER = webdriver.Chrome(chrome_options=OPTIONS, executable_path=ChromeDriverManager().install())
+CONFIG = jthon.load('config')
 
 #Config
-parser = OptionParser()
-now = datetime.datetime.now()
-
+PARSER = OptionParser()
 #-----functions-----
 
 def is_passchannel(ctx): #only exists to control usage
     """Checks if the channel is the password checking channel."""
-    #channelid = config.get('channelid')
-    #if not channelid:
-    #    channelid = '678708790878797825'
-    #return ctx.message.channel.id == channelid
     return ctx.message.channel.id == 678708790878797825
 
 def teardown_method():
-    driver.quit()
+    DRIVER.quit()
 
 def test_testloginzuck(password):
-    driver.get("https://zuckwatch.com/")
+    DRIVER.get("https://zuckwatch.com/")
     t.sleep(4)
-    Sel_password = driver.find_element_by_css_selector('#__layout > div > div > div > div > div.columns > div:nth-child(1) > div > input')
-    Sel_Login = driver.find_element_by_css_selector('#__layout > div > div > div > div > div.columns > div:nth-child(1) > div > a')
-    Sel_password.send_keys(password)
-    Sel_Login.click()
+    sel_password = DRIVER.find_element_by_css_selector('#__layout > div > div > div > div > div.columns > div:nth-child(1) > div > input')
+    sel_login = DRIVER.find_element_by_css_selector('#__layout > div > div > div > div > div.columns > div:nth-child(1) > div > a')
+    sel_password.send_keys(password)
+    sel_login.click()
 
 #-----class & commands-----
 
@@ -69,7 +47,7 @@ class Zuckwatch(commands.Cog):
         async with ctx.typing():
             test_testloginzuck(passwordtry)
             try:
-                WebDriverWait(driver, 1500).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, ".nuxt-progress-failed")))
+                WebDriverWait(DRIVER, 1500).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, ".nuxt-progress-failed")))
             except:
                 await ctx.send("**The password is:** " + passwordtry)
             finally:
