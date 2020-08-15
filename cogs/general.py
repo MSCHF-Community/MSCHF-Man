07a5f540d84b72e0142da15f0abc243a35896911
Brendon Barnes <brenbrenofspace@gmail.com>
2020-08-14 20:19:16 -0400

    change syncing

--- cogs/general.py
+++ cogs/general.py
@@ -9,11 +9,5 @@ class General(commands.Cog):
     async def repeat(self, ctx, *, arg): #it repeats what you feed it, got it?
         await ctx.send(f"{arg}")
 
-    @commands.command()
-    async def credits(self, ctx):
-        config = jthon.load('./config')
-        contributors = list(config.get('contributors').data)
-        await ctx.send(contributors)
-
 def setup(bot):
     bot.add_cog(General(bot))
