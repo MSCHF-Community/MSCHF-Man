f051100a95e7bc1baa63160f9e3ced9b56811c51
Brendon <brenbrenofspace@gmail.com>
2020-08-14 20:08:54 -0400

    Credits test commands implemented.

--- cogs/general.py
+++ cogs/general.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import jthon
 
 class General(commands.Cog):
     def __init__(self, bot):
@@ -7,6 +8,12 @@ class General(commands.Cog):
     @commands.command()
     async def repeat(self, ctx, *, arg): #it repeats what you feed it, got it?
         await ctx.send(f"{arg}")
+
+    @commands.command()
+    async def credits(self, ctx):
+        config = jthon.load('./config')
+        contributors = list(config.get('contributors').data)
+        await ctx.send(contributors)
 
 def setup(bot):
     bot.add_cog(General(bot))
