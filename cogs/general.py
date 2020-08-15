c6f56a3172282dc26465378597c27f4b7dafc839
Brendon Barnes <brenbrenofspace@gmail.com>
2020-08-14 20:22:01 -0400

    attempt to resync

--- cogs/general.py
+++ cogs/general.py
@@ -9,5 +9,11 @@ class General(commands.Cog):
     async def repeat(self, ctx, *, arg): #it repeats what you feed it, got it?
         await ctx.send(f"{arg}")
 
+    @commands.command()
+    async def credits(self, ctx):
+        config = jthon.load('./config')
+        contributors = list(config.get('contributors').data)
+        await ctx.send(contributors)
+
 def setup(bot):
     bot.add_cog(General(bot))
